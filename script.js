aa40920b16fef565797e4332fade44413e6778ab
Caio Lucas <clucaszrtmain@gmail.com>
2025-10-09 16:54:22 -0300

    refactor(Js): renomeação de váriaveis e funções do javascript

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-let contador = 0;
-let btn_addTarefa = document.querySelector(".btn-add") 
-let inputTarefa = document.querySelector("input") 
+let contador_Ids_Tasks = 0;
+let btn_AddTask = document.querySelector(".btn-add") 
+let inputTask = document.querySelector("input") 
 
-btn_addTarefa.addEventListener("click", adicionarTarefa) 
+btn_AddTask.addEventListener("click", adicionarTarefa) 
 
 
 
         
 function adicionarTarefa(){
-    let escopoUl = document.querySelector("ul") // Criei uma váriavel que irá armazenar a tag ul
-    let valorInputTarefa = inputTarefa.value // atribui a esta váriavel o valor do input
-    contador += 1;
+    let container_TaskList = document.querySelector("ul") // Criei uma váriavel que irá armazenar a tag ul
+    let valor_Input_Task = inputTask.value // atribui a esta váriavel o valor do input
+    contador_Ids_Tasks += 1;
     
-    if(valorInputTarefa != ''){
-        let itemLi = escopoUl.appendChild(document.createElement("li"))
-        let last_task = escopoUl.lastElementChild
-        itemLi.innerText = valorInputTarefa;
-        itemLi.setAttribute('id', contador)
-        let btn_remove = last_task.appendChild(document.createElement("button"))
-        btn_remove.textContent = "X"
-        btn_remove.setAttribute('class', `btn_remover${contador}`)
-        btn_remove.addEventListener("click", removerTarefa) //Não entendi
-
+    if(valor_Input_Task != ''){
+        let task_Li = container_TaskList.appendChild(document.createElement("li"))
+        let last_task = container_TaskList.lastElementChild
+        task_Li.innerText = valor_Input_Task;
+        task_Li.setAttribute('id', contador_Ids_Tasks)
+        let btn_Remove_Task = last_task.appendChild(document.createElement("button"))
+        btn_Remove_Task.textContent = "X"
+        btn_Remove_Task.setAttribute('class', `btn_remover${contador_Ids_Tasks}`)
+        btn_Remove_Task.addEventListener("click", removerTarefa)
     }else{
         alert("Coloque uma tarefa válida")
     }    
@@ -29,10 +28,10 @@ function adicionarTarefa(){
 
 
 function removerTarefa(evento){
-    let botaoClicado = evento.target; // Não entendi
-    let elementoPai = botaoClicado.parentNode;
-    let escopoUl = document.querySelector("ul")
-    escopoUl.removeChild(elementoPai)
+    let btn_Remove_Task = evento.target; 
+    let container_Task_Li = btn_Remove_Task.parentNode;
+    let container_TaskList = document.querySelector("ul")
+    container_TaskList.removeChild(container_Task_Li)
 }
 
         
