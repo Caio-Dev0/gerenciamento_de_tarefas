74778f1c7c240553afd8bc7b76263dbc376e4b60
Caio Lucas <clucaszrtmain@gmail.com>
2025-10-10 22:36:03 -0300

    refactor(js): Criada função isolada para receber o valor do input da task

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 let contador_Ids_Tasks = 0;
 let btn_AddTask = document.querySelector(".btn-add") 
 let inputTask = document.querySelector("input") 
+let container_TaskList = document.querySelector("ul") 
 
 btn_AddTask.addEventListener("click", addTask) 
 
-
+function recebeInput(){
+    let valor_Input_Task = inputTask.value 
+    valor_Input_Task = valor_Input_Task.trim()
+    return valor_Input_Task
+}
 
         
 function addTask(){
-    let container_TaskList = document.querySelector("ul") // Criei uma váriavel que irá armazenar a tag ul
-    let valor_Input_Task = inputTask.value // atribui a esta váriavel o valor do input
-    valor_Input_Task = valor_Input_Task.trim()
+    const valorInput = recebeInput()
+    console.log(valorInput)
     contador_Ids_Tasks += 1;
 
-    if(valor_Input_Task != ''){
+    if(valorInput != ''){
         let task_Li = container_TaskList.appendChild(document.createElement("li"))
         let last_task = container_TaskList.lastElementChild
-        task_Li.innerText = valor_Input_Task;
+        task_Li.innerText = valorInput;
         inputTask.value = ''
         task_Li.setAttribute('id', contador_Ids_Tasks)
         let btn_Remove_Task = last_task.appendChild(document.createElement("button"))
         btn_Remove_Task.textContent = "X"
         btn_Remove_Task.setAttribute('class', `btn_remover${contador_Ids_Tasks}`)
         btn_Remove_Task.addEventListener("click", deleteTask)
+        
     }else{
         alert("Coloque uma tarefa válida")
     }    
+    
 }
 
 
 function deleteTask(evento){
     let btn_Remove_Task = evento.target; 
     let container_Task_Li = btn_Remove_Task.parentNode;
-    let container_TaskList = document.querySelector("ul")
     container_TaskList.removeChild(container_Task_Li)
 }
 
