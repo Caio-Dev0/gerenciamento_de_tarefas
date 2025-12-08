37da4014f4effcc41a2c57485935a391bdb79c83
Caio Lucas <clucaszrtmain@gmail.com>
2025-12-08 18:58:21 -0300

    style: Adiciona um link de icones do tabler icons e muda o botão de remover e editar tarefa para icones que representam suas funcionalidades, melhorando a UI

--- js/modules/manipulaDom.js
+++ js/modules/manipulaDom.js
@@ -12,17 +12,19 @@ function criaItemTarefa(conteudo, id, deletarTarefa, editarTarefa, categoria){
 }
 
 function criaBotaoRemover(deletarTarefa){
-    const botaoRemoverTarefa = document.createElement("button")
-    botaoRemoverTarefa.textContent = "X"
+    const botaoRemoverTarefa = document.createElement("i")
     botaoRemoverTarefa.classList.add("btn-remover")
+    botaoRemoverTarefa.classList.add("ti")
+    botaoRemoverTarefa.classList.add("ti-square-rounded-x")
     botaoRemoverTarefa.addEventListener("click", deletarTarefa)
     return botaoRemoverTarefa;
 }
 
 function criaBotaoEditar(editarTarefa){
-    const botaoEditarTarefa = document.createElement("button")
-    botaoEditarTarefa.textContent = "🪶"
+    const botaoEditarTarefa = document.createElement("i")
     botaoEditarTarefa.classList.add("btn-edit")
+    botaoEditarTarefa.classList.add("ti")
+    botaoEditarTarefa.classList.add("ti-edit")
     botaoEditarTarefa.addEventListener("click", editarTarefa)
     return botaoEditarTarefa;
 }
